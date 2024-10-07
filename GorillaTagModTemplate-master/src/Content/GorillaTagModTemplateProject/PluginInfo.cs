30c97501d9b713684bec832bb79972baf6af7b93
Inferno On VR <infernoonvr@gmail.com>
2024-10-06 21:29:23 -0400

    Add files via upload

---
+++ GorillaTagModTemplate-master/src/Content/GorillaTagModTemplateProject/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaTagModTemplateProject
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.infernoonvr.gorillatag.infernoscammod";
+		public const string Name = "infernocammod";
+		public const string Version = "1.0.0";
+	}
+}
